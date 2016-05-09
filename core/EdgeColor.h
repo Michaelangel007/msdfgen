e639fb4c73a594ae84b7541cc9868b1d19ad78ca
Michaelangel007 <Michaelangel007@sharedcraft.com>
2016-05-09 15:57:24 -0700

    Fix alignment EdgeColor.h

--- core/EdgeColor.h
+++ core/EdgeColor.h
@@ -5,14 +5,14 @@ namespace msdfgen {
 
 /// Edge color specifies which color channels an edge belongs to.
 enum EdgeColor {
-    BLACK = 0,
-    RED = 1,
-    GREEN = 2,
-    YELLOW = 3,
-    BLUE = 4,
+    BLACK   = 0,
+    RED     = 1,
+    GREEN   = 2,
+    YELLOW  = 3,
+    BLUE    = 4,
     MAGENTA = 5,
-    CYAN = 6,
-    WHITE = 7
+    CYAN    = 6,
+    WHITE   = 7
 };
 
 }
